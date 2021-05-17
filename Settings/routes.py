e9481dbd1e551ac9d3f3a74821d6464c2d78622f
jacopoesposito <jaco.espo@hotmail.it>
2021-05-17 02:04:16 +0200

    Added the modify password form into the security center, now users can change their password

--- Settings/routes.py
+++ Settings/routes.py
@@ -1,11 +1,22 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user
 
-from __init__ import login_required, db
+from Settings.forms import passwordChangeForm
+from __init__ import login_required, db, bcrypt
+from models import Users
 
 settings = Blueprint('Settings', __name__)
 
 @settings.route('/securitycenter', methods=['GET', 'POST'])
 @login_required
 def securityCenter():
-    return render_template('securitycenter.html')
+
+    changePassform = passwordChangeForm()
+    if changePassform.validate_on_submit():
+        if bcrypt.check_password_hash(current_user.PASSWORD, changePassform.oldPassword.data):
+            newPass = bcrypt.generate_password_hash(changePassform.newPassword.data).decode('utf-8')
+            current_user.PASSWORD = newPass
+            db.session.flush()
+            db.session.commit()
+        return render_template('securitycenter.html', changePassform=changePassform)
+    return render_template('securitycenter.html', changePassform=changePassform)
